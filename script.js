d7399535d989b4732a990bcdc465ad6df9ac3c8a
Maja Gedda <maja.gedda@zocom.se>
2020-10-22 12:02:09 +0200

    init repo

---
+++ script.js
@@ -0,0 +1,35 @@
+let array = [
+    {
+        name: "Felicia",
+        age: 12
+    },
+    {
+        name: "Pelle",
+        age: 20
+    },
+    {
+        name: "Peter",
+        age: 59
+    },
+    {
+        name: "Anna",
+        age: 32
+    },
+    {
+        name: "Jocke",
+        age: 18
+    },
+    {
+        name: "Ella",
+        age: 3
+    },
+]
+
+//Del 1
+//Skriv ut alla namn som är över 18
+
+//Del 2
+//Skriv ut alla som är under 18
+
+//Del 3
+//Sortera objekten efter ålder, minst först
